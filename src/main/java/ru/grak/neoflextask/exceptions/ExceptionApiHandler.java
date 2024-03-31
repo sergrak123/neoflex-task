db057036cfad74c7af8a826594834f8651e6878c
Sergey <sergey-707@mail.ru>
2024-03-31 23:17:12 +0400

    fix exception handler

--- src/main/java/ru/grak/neoflextask/exceptions/ExceptionApiHandler.java
+++ src/main/java/ru/grak/neoflextask/exceptions/ExceptionApiHandler.java
@@ -3,10 +3,12 @@ package ru.grak.neoflextask.exceptions;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,5 +32,27 @@ public class ExceptionApiHandler {
                 .collect(Collectors.toList());
 
         return new ValidationErrorResponse(violations);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public Violation onMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e) {
+
+        String paramName = e.getName();
+        String paramValue = e.getValue().toString();
+        String errorMessage = "Failed to convert parameter '" + paramName + "' with value '" + paramValue + "' to required type";
+
+        return new Violation(paramName, errorMessage);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public Violation onMissingServletRequestParameterException(
+            MissingServletRequestParameterException e) {
+
+        String paramName = e.getParameterName();
+        String errorMessage = "Required parameter '" + paramName + "' is not present";
+        return new Violation(paramName,errorMessage);
     }
 }
