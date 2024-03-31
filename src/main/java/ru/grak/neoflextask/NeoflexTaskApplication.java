782ecb00354b456c16f83d2b2fb7a72caf649bb6
Sergey <sergey-707@mail.ru>
2024-03-31 14:52:53 +0400

    Initial commit

---
+++ src/main/java/ru/grak/neoflextask/NeoflexTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.grak.neoflextask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoflexTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/grak/neoflextask/NeoflextaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.grak.neoflextask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflextaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
